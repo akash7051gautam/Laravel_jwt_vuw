34205a1fdc3853c39ca1dd025fc182ed027654cf
akash7051gautam <akashdeep.gautam@ditstek.com>
2021-06-11 11:56:28 +0530

    first commit

---
+++ laravel_jwt_vuejs/app/Http/Controllers/AuthController.php
@@ -0,0 +1,110 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Auth;
+use Validator;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user
+     */
+    public function register(Request $request)
+    {
+        $v = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users',
+            'password'  => 'required|min:3|confirmed',
+        ]);
+        if ($v->fails())
+        {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $v->errors()
+            ], 422);
+        }
+        
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        $credentials = $request->only('email','password');
+        $token = JWTAuth::attempt($credentials);
+
+        return $this->respondWithToken($token);
+
+        //return response()->json(['status' => 'success'], 200);
+    }
+    /**
+     * Login user and return a token
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+            try {
+                if (! $token = JWTAuth::attempt($credentials)) {
+                    return response()->json(['error' => 'invalid_credentials'], 400);
+                }
+            } catch (JWTException $e) {
+                return response()->json(['error' => 'could_not_create_token'], 500);
+            }
+
+            return $this->respondWithToken($token);
+    }
+    /**
+     * Logout User
+     */
+    public function logout()
+    {
+        $this->guard()->logout();
+        return response()->json([
+            'status' => 'success',
+            'msg' => 'Logged out Successfully.'
+        ], 200);
+    }
+    /**
+     * Get authenticated user
+     */
+    public function user(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+    /**
+     * Refresh JWT token
+     */
+    public function refresh()
+    {
+        if ($token = $this->guard()->refresh()) {
+            return response()
+                ->json(['status' => 'successs'], 200)
+                ->header('Authorization', $token);
+        }
+        return response()->json(['error' => 'refresh_token_error'], 401);
+    }
+    /**
+     * Return auth guard
+     */
+    private function guard()
+    {
+        return Auth::guard();
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => auth()->user(),
+            //'expires_in' => auth()->factory('api')->getTTL() * 60
+        ]);
+    }
+}
